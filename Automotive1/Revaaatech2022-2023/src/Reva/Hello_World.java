fe79a3b80407464a3f66e0511214c83c5c777a42
Arohi <Arohi@LAPTOP-TAU72GUO>
2022-09-30 13:00:09 +0530

    commit

---
+++ Automotive1/Revaaatech2022-2023/src/Reva/Hello_World.java
@@ -0,0 +1,14 @@
+package Reva;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello World");
+      
+      System.out.print("Hello World");
+      
+      
+	}
+
+}
